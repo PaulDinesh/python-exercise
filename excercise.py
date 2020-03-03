c044c1f4aa4af82e7623754e63eb0634abb66aa6
PaulDinesh <44960323+PaulDinesh@users.noreply.github.com>
2020-03-03 10:16:28 +0000

    Create excercise.py

---
+++ excercise.py
@@ -0,0 +1,19 @@
+prices=10
+class portfolio(object):
+    def __init__(self,symbl,qnty):
+        self.symbl=symbl
+        self.qnty=qnty
+    
+    def price():
+        print(prices)
+    
+    def invest():
+        
+    def divest():
+        
+    def chkqnty():
+        print(prices)
+            
+    def value():
+        
+portfol= portfolio("USD",1)
